28610119e52a29ff37ead3ce230387be62ca855f
Lars Schneider <Lars.sschneider@outlook.com>
2021-07-29 09:25:32 -0700

    Comments.

--- Oak/src/Core/Main.cpp
+++ Oak/src/Core/Main.cpp
@@ -6,15 +6,18 @@ using namespace Oak;
 
 int main(int argc, char** argv)
 {
+	//Ecs initialization.
 	ECS_Manager* ecs = new ECS_Manager();
 	Oak::Lua_ECS_instance = new Oak::ECS_Instace();
 	Oak::Lua_ECS_instance->manager = ecs;
 
+	//Main entity
 	Entity player = ecs->Create_Entity();
 	ecs->Add_Tag(player, { "Player" });
 	ecs->Add_Position(player, { 100,100 });
 	ECS_Lua_Add_Script(player);
 
+	//To prove that there can be more than one entity.
 	Entity p2 = ecs->Create_Entity();
 	ecs->Add_Tag(p2, { "Player 2" });
 	ecs->Add_Position(p2, { 54,54 });
